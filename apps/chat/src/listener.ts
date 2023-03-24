a9871b86786dac386ea700ba657a1fc86b55c2f8
arcinston <arushkurundodi@gmail.com>
2023-03-24 18:54:47 +0530

    wip: typesafe chat

--- apps/chat/src/listener.ts
+++ apps/chat/src/listener.ts
@@ -1,39 +1,37 @@
-/* eslint-disable no-console */
+import { createFromJSON } from '@libp2p/peer-id-factory';
 
-import { createLibp2p } from './libp2p.js'
-import { stdinToStream, streamToConsole } from './stream.js'
-import { createFromJSON } from '@libp2p/peer-id-factory'
-import peerIdListenerJson from './peer-id-listener.js'
+import { createLibp2p } from './libp2p.js';
+import peerIdListenerJson from './peer-id-listener.js';
+import { stdinToStream, streamToConsole } from './stream.js';
 
-async function run () {
+async function run() {
   // Create a new libp2p node with the given multi-address
-  const idListener = await createFromJSON(peerIdListenerJson)
+  const idListener = await createFromJSON(peerIdListenerJson);
   const nodeListener = await createLibp2p({
     peerId: idListener,
     addresses: {
-      listen: ['/ip4/0.0.0.0/tcp/10333']
-    }
-  })
-
+      listen: ['/ip4/0.0.0.0/tcp/10333'],
+    },
+  });
   // Log a message when a remote peer connects to us
-  nodeListener.connectionManager.addEventListener('peer:connect', (evt) => {
-    const connection = evt.detail
-    console.log('connected to: ', connection.remotePeer.toString())
-  })
+  nodeListener.addEventListener('peer:connect', evt => {
+    const connection = evt.detail;
+    console.log('connected to: ', connection.remotePeer.toString());
+  });
 
   // Handle messages for the protocol
   await nodeListener.handle('/chat/1.0.0', async ({ stream }) => {
     // Send stdin to the stream
-    stdinToStream(stream)
+    stdinToStream(stream);
     // Read the stream and output to console
-    streamToConsole(stream)
-  })
+    streamToConsole(stream);
+  });
 
   // Output listen addresses to the console
-  console.log('Listener ready, listening on:')
-  nodeListener.getMultiaddrs().forEach((ma) => {
-    console.log(ma.toString())
-  })
+  console.log('Listener ready, listening on:');
+  nodeListener.getMultiaddrs().forEach(ma => {
+    console.log(ma.toString());
+  });
 }
 
-run()
+run();
